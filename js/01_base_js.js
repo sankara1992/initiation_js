217945a92ff8a740eb62bb131d85d769b3315281
sankara1992 <58479660+sankara1992@users.noreply.github.com>
2019-12-18 17:05:31 +0100

    cours de mercredi

---
+++ js/01_base_js.js
@@ -0,0 +1,23 @@
+/***********
+ SYNTAXE DE BASE
+
+ ************/
+// 1. je vérifie toujours que ma page js et mon fichier html sont connectés :
+alert("okay js");
+
+// deux slasches pour un commentaire sur une ligne (ctrl + /)
+/*
+(shift + alt+a)
+ici un 
+commentaire sur 
+plusieurs lignes 
+
+*/
+
+/*
+
+2 manière d'afficher une boite de dialogue :
+
+   *> alert('super, tu es arrivé sur mon site !');
+   *> prompt('question : on est quel jour ?'); avec la fonction prompt je dialogue directement avec l'utilisateur
+   */
